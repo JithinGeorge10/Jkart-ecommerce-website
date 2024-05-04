66c9a920aec1c64eb7bff31d6c49fe9522485108
JithinGeorge10 <jithin.george.el@gmail.com>
2024-05-04 17:25:40 +0530

    commit

--- controller/cartController.js
+++ controller/cartController.js
@@ -304,10 +304,6 @@ const phonePay = async (req, res) => {
         console.log('error caught');
         console.log(err);
     }
-
-
-
-    
 }
 
 
--- helper/offer.js
+++ helper/offer.js
@@ -1,12 +1,38 @@
+const ProductModel = require("../model/productModel");
+const ProductOfferModel = require("../model/productOfferModel");
 
 
-
-const applyProductOffer = async (req, res) => {
+const applyProductOffer = async () => {
     try {
-       console.log('offer');
-        
-    } catch (err) {
-        console.log(err);
+      const today = new Date();
+  
+      const offers = await ProductOfferModel.find().populate("productId");
+      console.log(offers);
+  
+      offers.forEach(async (offer) => {
+        if (offer.productId.offerPrice  === offer.productId.productPrice) {
+          if (
+            offer.startDate <= today &&
+            offer.endDate >= today &&
+            offer.currentStatus
+          ) {
+            await ProductModel.findByIdAndUpdate(offer.productId, {
+              offerPrice: Math.floor(
+                offer.productId.productPrice -
+                  (offer.productId.productPrice * offer.productOfferPercentage) / 100
+              ),
+            });
+          } else {
+            await ProductModel.findByIdAndUpdate(offer.productId, {
+              offerPrice: offer.productId.productPrice,
+            });
+          }
+        }
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+
+
 module.exports =  applyProductOffer ;
